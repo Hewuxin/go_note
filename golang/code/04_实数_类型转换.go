ea9fc3782c09a731d33f489200763355136b6584
Hewuxin <2386474352@qq.com>
2022-04-09 10:55:40 +0800

    22-4-9

--- golang/code/04_实数_类型转换.go
+++ golang/code/04_实数_类型转换.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	message := "shalom"
-	for count := 0; count < len(message); count++ {
-		fmt.Printf("%c\n", message[count])
-	}
+	// message := "shalom"
+	// for count := 0; count < len(message); count++ {
+	// 	fmt.Printf("%c\n", message[count])
+	// }
+	fmt.Println(math.Abs(-19))                // 绝对值
+	fmt.Println(math.Floor(3.14))             // 向下取整
+	fmt.Println(math.Ceil(3.14))              // 向上取整
+	fmt.Println(math.Round(3.34780))          // 就近取整
+	fmt.Println(math.Round(3.5478*100) / 100) // 保留小数点后两位
+	fmt.Println(math.Mod(11, 3))              // 取余数 11%3
+	fmt.Println(math.Pow(2, 5))               // 计算次方 2的5次方
+	fmt.Println(math.Pow10(2))                //计算10次方 如2的10放
+	fmt.Println(math.Max(1, 2))               // 两个值，取最大值
+	fmt.Println(math.Min(1, 2))               // 两个值，取最小值
 }
--- golang/code/practice_01.go
+++ golang/code/practice_01.go
@@ -1,9 +1,3 @@
-package main
-
-import (
-	"fmt"
-)
-
 /*
 func practice01() {
 	fmt.Println("This is practice_01!")
@@ -117,16 +111,50 @@ func main() {
 // 	return a + b
 // }
 
-func speed(distance, time float64) float64 {
-	// 
-	return distance / time
-}
+// func speed(distance, time float64) float64 {
+// 	//
+// 	return distance / time
+// }
+// func main() {
+// 	// vary_const_var()
+// 	// yunsuan()
+// 	// auto_increment()
+// 	// rand_num()
+// 	const distance = 56000000.0
+// 	const time = 28 * 24.0
+// 	fmt.Println("speed = ", speed(distance, time))
+// }
+
+// var name, address, action string
+// fmt.Print("请输入名称: ")
+// fmt.Scanln(&name)
+
+// fmt.Print("请输入位置: ")
+// fmt.Scanln(&address)
+
+// fmt.Print("请输入动作: ")
+// reader := bufio.NewReader(os.Stdin)
+// line, _, _ := reader.ReadLine()
+// action = string(line)
+
+// result := fmt.Sprintf("我叫 %s  在%s 在 %s", name, address, action)
+// fmt.Println(result)
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
-	// vary_const_var()
-	// yunsuan()
-	// auto_increment()
-	// rand_num()
-	const distance = 56000000.0
-	const time = 28 * 24.0
-	fmt.Println("speed = ", speed(distance, time))
+	fmt.Println(math.Abs(-19))                // 绝对值
+	fmt.Println(math.Floor(3.14))             // 向下取整
+	fmt.Println(math.Ceil(3.14))              // 向上取整
+	fmt.Println(math.Round(3.34780))          // 就近取整
+	fmt.Println(math.Round(3.5478*100) / 100) // 保留小数点后两位
+	fmt.Println(math.Mod(11, 3))              // 取余数 11%3
+	fmt.Println(math.Pow(2, 5))               // 计算次方 2的5次方
+	fmt.Println(math.Pow10(2))                //计算10次方 如2的10放
+	fmt.Println(math.Max(1, 2))               // 两个值，取最大值
+	fmt.Println(math.Min(1, 2))               // 两个值，取最小值
 }
